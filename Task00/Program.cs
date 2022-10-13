0bc2bfe6e29d2682ede8b49a20f51ce850818014
Vyacheslav Kurbatov <Kurbatov-Slavard@yandex.ru>
2022-10-13 11:22:14 +0300

    add Task00 решение

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+// 1. на вход принимает число 
+// 2. выдаёт его квадрат (число умноженное на само себя).
+
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine()); // Convert.ToInt32 конвертирует из типа данных sring в int т.к. Readline по умолчанию считвает как string
+
+// string str = Console.ReadLine();
+// int number = Convert.ToInt32 (str);
+
+int suare = number * number;
+Console.WriteLine($"Квадрат числа {number} = {suare}");
