743d5be2b4b217bdd31dda8f2fd9b1bea4cf4807
Vyacheslav Kurbatov <Kurbatov-Slavard@yandex.ru>
2022-10-13 12:18:06 +0300

    add Task03 решение

---
+++ Task03/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("Введите номер дня недели ");
+int day = Convert.ToInt32(Console.ReadLine());
+if(day > 0 && day < 8)
+{
+    if(day == 1) 
+    {
+        Console.WriteLine("Понедельник");
+    }
+    else 
+    {
+        if(day == 2) 
+        {
+        Console.WriteLine("Вторник");
+        }
+        else 
+        {
+            if(day == 3) 
+            {
+            Console.WriteLine("Вторник");
+            }
+            else {
+                if(day == 4) 
+                {
+                Console.WriteLine("Четверг");
+                }
+                else 
+                {
+                    if(day == 5) 
+                    {
+                    Console.WriteLine("Пятница");
+                    }
+                    else 
+                    {
+                        if(day == 6) 
+                        {
+                        Console.WriteLine("Суббота");
+                        }
+                        else 
+                        {
+                            if(day == 7) 
+                            {
+                            Console.WriteLine("Воскресенье");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    
+    }
+}
+else 
+{
+    Console.WriteLine("Не день недели");
+}
