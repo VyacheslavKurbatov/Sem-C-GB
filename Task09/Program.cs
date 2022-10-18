e2fb49b118caed6b48da4d63208462dcff3851cb
Vyacheslav Kurbatov <Kurbatov-Slavard@yandex.ru>
2022-10-18 11:08:21 +0300

    add Tusk09 plus Method

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -9,25 +9,36 @@
 int number = new Random().Next(10, 100); // последнее число не включается в диапозон, а первое включено
 Console.WriteLine($"Случайное число из отрезка 10 - 99 => {number}");
 
-int firstDigit = number / 10; // 78/10 = 7 т.к. целочисленное деление
-int secondDigit = number % 10; // 78%10 = 8 т.к. это остаток от деления
+// int firstDigit = number / 10; // 78/10 = 7 т.к. целочисленное деление
+// int secondDigit = number % 10; // 78%10 = 8 т.к. это остаток от деления
 
-// условный оператор
-// int result = default; // default это дефолтное значение переменной (в данном случае у int это 0)
-// if(firstDigit > secondDigit) result = firstDigit;
-// else result = secondDigit;
-// Console.WriteLine($"Наибольшая цифра числа {result}");
+// // условный оператор
+// // int result = default; // default это дефолтное значение переменной (в данном случае у int это 0)
+// // if(firstDigit > secondDigit) result = firstDigit;
+// // else result = secondDigit;
+// // Console.WriteLine($"Наибольшая цифра числа {result}");
 
-// if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {firstDigit}");
-// else Console.WriteLine($"Наибольшая цифра числа {secondDigit}");
+// // if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {firstDigit}");
+// // else Console.WriteLine($"Наибольшая цифра числа {secondDigit}");
 
-// тернарный оператор 1
-int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit; // (тернарный оператор) если первое число больше второго тогда мы записываем в переменную max первую цифру, иначе записываем в переменную max вторую цифру
-Console.WriteLine($"Наибольшая цифра числа {maxDigit}");
+// // тернарный оператор 1
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit; // (тернарный оператор) если первое число больше второго тогда мы записываем в переменную max первую цифру, иначе записываем в переменную max вторую цифру
+// Console.WriteLine($"Наибольшая цифра числа {maxDigit}");
 
-// int res = Math.Max(FirstDigit, SecondDigit);  // встроенная математическая функция
-// Console.WriteLine(res);
+// // int res = Math.Max(FirstDigit, SecondDigit);  // встроенная математическая функция
+// // Console.WriteLine(res);
 
-// тернарный оператор 2
-Console.Write($"Наибольшая цифра числа {number} равна ");
-Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit); // в таком случае не нужна новая переменная
+// // тернарный оператор 2
+// Console.Write($"Наибольшая цифра числа {number} равна ");
+// Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit); // в таком случае не нужна новая переменная
+
+
+int MaxDigit(int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+int maxDigit = MaxDigit(number);
+Console.Write($"Наибольшая цифра числа равна {maxDigit}");
