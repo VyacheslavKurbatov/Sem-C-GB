2117406ee192741233b043ca45ed936792cb01c4
Vyacheslav Kurbatov <Kurbatov-Slavard@yandex.ru>
2022-10-18 11:33:44 +0300

    add Task11 solution

---
+++ Task11/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 11. Напишите программу, которая 
+// 1. выводит случайное трёхзначное число и 
+// 2. удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число => {number}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    return (firstDigit * 10) + thirdDigit;
+}
+
+int ftnumber = MaxDigit(number);
+Console.Write($"Первая и третья цифра числа {number} -> {ftnumber}");
