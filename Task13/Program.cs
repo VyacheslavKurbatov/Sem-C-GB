add1ee7ae6a8d460a2d6a5f332c5edbff9f3951e
Vyacheslav Kurbatov <Kurbatov-Slavard@yandex.ru>
2022-10-20 09:34:20 +0300

    add Tusk13 solution

---
+++ Task13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 13: Напишите программу, которая 
+// 1. выводит третью цифру заданного числа 
+// 2. или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Эта программа показывает третью цифру числа.");
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digit = number;
+if (digit > 99 || digit < -99)
+{
+    if(digit < 0) 
+    {
+        digit = Math.Abs(digit);
+    }
+    while (digit > 1000)
+    {
+        digit = digit / 10;
+    }
+    digit = digit % 10;
+    Console.Write($"Третья цифра числа {number} -> {digit}");
+}
+else Console.Write("Третьей цифры нет");
