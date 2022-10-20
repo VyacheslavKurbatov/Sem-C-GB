08c6a48d49d76a78d24aa1974c725a871f029199
Vyacheslav Kurbatov <Kurbatov-Slavard@yandex.ru>
2022-10-20 09:48:40 +0300

    add Tusk15 solution

---
+++ Task15/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 15: Напишите программу, которая 
+// 1. принимает на вход цифру, обозначающую день недели, и 
+// 2. проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Эта программа показывает является ли этот день выходным.");
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number <= 7 && number > 0)
+{
+    bool Weekend(int num)
+    {
+        if (num == 6 || num == 7) return true;
+    return false;
+    }
+    if (Weekend(number))
+    {
+        Console.WriteLine($"{number} -> да");
+    }
+    else Console.WriteLine($"{number} -> нет");
+}
+else Console.WriteLine("Не день недели");
