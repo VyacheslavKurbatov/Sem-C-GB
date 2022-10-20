02adee3ab77c098e389eb22f4f53e4cf6e871007
Vyacheslav Kurbatov <Kurbatov-Slavard@yandex.ru>
2022-10-20 12:29:40 +0300

    add Task22

---
+++ Tusk22/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 22: Напишите программу, которая
+// 1. принимает на вход число (N) и 
+// 2. выдаёт таблицу квадратов чисел от 1 до N.
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// void Square(int num)
+// {
+//     int count = 1;
+//     while(count<=num)
+//     {
+//         int sqrt = count * count;
+//         Console.WriteLine($"_{count}_|_{sqrt}_");
+//         count++;
+//     }
+// }
+
+// Square(number);
+
+
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Square(int num)
+{
+    
+    if (num > 0)
+    {
+        for (int i = 1; i <= num; i++)
+        {
+            Console.WriteLine($"{i,4} | {i*i,4}");
+        }
+    }
+    else 
+    {
+        Console.WriteLine($"введите значение больше 0");
+    }
+}
+
+Square(number);
