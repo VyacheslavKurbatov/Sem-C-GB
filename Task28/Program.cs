8d8bda99ea73e18255ca8bbd30b784f72e29369a
Vyacheslav Kurbatov <Kurbatov-Slavard@yandex.ru>
2022-11-07 12:40:02 +0300

    add Task28 and Task30 solution

---
+++ Task28/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 28: Напишите программу, которая
+// 1. принимает на вход число N и 
+// 2. выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Faktorial(int number)
+{
+    int multiply = 1;
+    for (int i = 1; i <= number; i++)
+        {
+            multiply *= i;
+        }
+    return multiply;
+}
+
+int faktorial = Faktorial(num);
+Console.WriteLine($"Произведение чисел {num} -> {faktorial}");
---
+++ Task30/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+Console.Write("Введите количество элементов массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+int[] NameArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+",");
+    }
+}
+int[] nameArray = NameArray(num);
+PrintArray(nameArray);
