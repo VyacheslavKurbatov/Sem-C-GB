e4efad4f5fc7cf7f7942ad48b2f4ff30ae701556
Vyacheslav Kurbatov <Kurbatov-Slavard@yandex.ru>
2022-11-07 11:28:24 +0300

    add Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 26: Напишите программу, которая 
+// 1. принимает на вход число и 
+// 2. выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int QuantityNumbers(int number)
+{
+    int count = default;
+    for (int i = 0; number >= 1; i++)
+    {
+        number = number / 10;
+        count = count + 1; 
+    }
+    return count;
+}
+
+int quantityNumberss = QuantityNumbers(num);
+Console.WriteLine($"Колличество чисел {num} -> {quantityNumberss}");
