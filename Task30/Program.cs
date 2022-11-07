a162c2da43065682d44b3da3bfbec8dbeb90a781
Vyacheslav Kurbatov <Kurbatov-Slavard@yandex.ru>
2022-11-07 12:53:53 +0300

    add Task30 solution with design

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -11,19 +11,26 @@ int num = Convert.ToInt32(Console.ReadLine());
 int[] NameArray(int size)
 {
     int[] array = new int[size];
+    Random rnd = new Random();
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(0, 2);
+        array[i] = rnd.Next(0, 2);
     }
     return array;
 }
 
 void PrintArray(int[] array)
 {
+    Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(array[i]+",");
+        Console.Write(array[i]);
+        if (i<array.Length-1) 
+        {
+            Console.Write(",");
+        }
     }
+    Console.Write("]");
 }
 int[] nameArray = NameArray(num);
 PrintArray(nameArray);
