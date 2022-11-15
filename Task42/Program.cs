4510d7ee2d45a8b20b1da3b777090637b667c7d4
Vyacheslav Kurbatov <Kurbatov-Slavard@yandex.ru>
2022-11-15 17:32:32 +0300

    add Task42 solution

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -15,6 +15,7 @@ int ConvertToBinarySystem(int num)
     {
         x = (x * 10) + (num % 2);
         num = (num / 2);
+        if (x == 0) x = 2;
     }
 
     for (int i = 1; x > 0; i++)
@@ -22,6 +23,11 @@ int ConvertToBinarySystem(int num)
         int remnant = x % 10;
         res = res * 10 + remnant;
         x = x / 10;
+    }
+    if (res % 10 == 2)
+    {
+        res /= 10;
+        res *= 10;
     }
     return res;
 }
@@ -61,3 +67,46 @@ Console.WriteLine($"{result}");
 // }
 
 // Console.WriteLine(RevertNumber(ConvertToBin(number)));
+
+
+
+
+
+
+
+// int ReverseNumber(int num2)
+// {
+//     int result = default;
+
+//     while (num2 > 9 || num2 > 0)
+//     {
+//         result += num2 % 10;
+//         result *= 10;
+//         num2 /= 10;
+//     }
+//     result += num2 % 10;
+
+//     return result;
+// }
+
+// System.Console.WriteLine("Введите число десятичной системы исчисления");
+// int number10 = Convert.ToInt32(Console.ReadLine());
+
+// int ConvertToBin(int num10)
+// {
+//     int num2 = default;
+
+//     while (num10 > 0)
+//     {
+//         num2 += num10 % 2;
+//         num2 *= 10;
+//         num10 /= 2;
+//         ConvertToBin(num10);
+//     }
+
+//     return num2;
+// }
+
+// int number2Rev = ConvertToBin(number10);
+// int number2 = ReverseNumber(number2Rev);
+// Console.WriteLine(number2);
